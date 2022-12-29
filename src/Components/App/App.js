3d62610695ca7c827f4fa433d28dfe7db788676e
Devon Stanton <devon.stanton@gmail.com>
2022-12-29 14:19:25 +0100

    search bar passing a value

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -49,6 +49,7 @@ class App extends Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -97,6 +98,10 @@ class App extends Component {
     });
   }
 
+  search(term){
+    console.log(term)
+  }
+
   render() {
     return (
       <div>
@@ -104,7 +109,7 @@ class App extends Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults
               tracks={this.state.searchResults}
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+
+        this.search = this.search.bind(this)
+        this.handleTermChange = this.handleTermChange.bind(this)
+    }
+
+    search() {
+        const term = this.state.term
+        this.props.onSearch(term)
+    }
+
+    handleTermChange(e){
+        const term = e.target.value
+        this.setState({
+            term: term
+        })
+    }
     render() {
         return(
         <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
+            <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+            <button className="SearchButton" onClick={this.search}>SEARCH</button>
         </div>
       )
     }
